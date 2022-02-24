19f1afdf24c032d5cc5011c058ab397526c958b1
Gabriela <gabriela0403rodriguesdossanto@gmail.com>
2022-02-24 16:08:04 -0300

    Requisito 5 - realizado

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ class App extends React.Component {
     super();
     this.onSaveButtonClick = this.onSaveButtonClick.bind(this);
     this.onInputChange = this.onInputChange.bind(this);
+    this.validateForm = this.validateForm.bind(this);
+
     this.state = {
       cardName: '',
       cardDescription: '',
@@ -26,13 +28,49 @@ class App extends React.Component {
     const { name } = target;
     const value = (target.type === 'checkbox') ? target.checked : target.value;
 
-    this.setState({
-      [name]: value });
+    this.setState({ [name]: value }, () => this.validateForm());
   }
 
   onSaveButtonClick(e) {
     e.preventDefault();
   }
+
+  validateForm() {
+    const {
+      cardName,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardImage,
+      cardRare,
+    } = this.state;
+    const numAtrrMax = 210;
+    const numAtrrMin = 0;
+    const numAtrrCardMax = 90;
+    const sum = Number(cardAttr1) + Number(cardAttr2) + Number(cardAttr3);
+    const card = (
+      cardName !== ''
+     && cardImage !== ''
+     && cardDescription !== ''
+     && cardRare !== ''
+    );
+    const attr1 = (cardAttr1 >= numAtrrMin && cardAttr1 <= numAtrrCardMax);
+    const attr2 = (cardAttr2 >= numAtrrMin && cardAttr2 <= numAtrrCardMax);
+    const attr3 = (cardAttr3 >= numAtrrMin && cardAttr3 <= numAtrrCardMax);
+    if (
+      card === true
+      && attr1 === true
+      && attr2 === true
+      && attr3 === true
+      && sum <= numAtrrMax
+    ) {
+      this.setState({ isSaveButtonDisabled: false });
+    } else {
+      this.setState({ isSaveButtonDisabled: true });
+    }
+  }
+ 
 
   render() {
     const {
